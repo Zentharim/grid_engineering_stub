bd9b95d4aadf861b5c63384a8b02ae1f4df7057b
Matteo Settembrini <settembrinimatteo@gmail.com>
2020-12-09 21:01:06 +0100

    Flow

--- bpy_test.py
+++ bpy_test.py
@@ -6,6 +6,118 @@ import geopandas
 from geopy.distance import geodesic as dist
 from shapely.geometry import LineString
 import argparse
+from matplotlib import pyplot as plt
+
+
+def create_parser():
+    l_parser = argparse.ArgumentParser()
+    # defaults are for Fiumicino for testing reasons. For Qatar:
+    # python bpy_test.py -i "./shp/qatar-line.shp" -s 56.441,25.533 56.525,24.8
+    # or for test with bounding box:
+    # python bpy_test.py -i "./shp/qatar-line.shp" -s 56.441,25.4 56.525,25.000 -b 56.332 25.4 56.525 25.000
+
+    l_parser.add_argument("-i", "--input_shapefile", help="Shapefile to use as input",
+                          default="./shp/fiumicino-line.shp")
+    l_parser.add_argument("-b", "--bbox", nargs="+",
+                          help="Bounding box for the shapefile. e.g. lon_up_sx lat_up_sx lon_down_dx lat_down_dx")
+    l_parser.add_argument("-s", "--sea_points", nargs="+",
+                          help="List of sea points where to close the dominion. e.g. lon1,lat1 lon2,lat2 ...",
+                          default=["11.992,41.872", "12.080,41.680"])
+    l_parser.add_argument("-t", "--threshold",
+                          help="Threshold for the minimum distance to merge shapefile parts and remove double points",
+                          default="0.001")
+    l_parser.add_argument("-o", "--output_shapefile", help="Shapefile to use as output",
+                          default="./shp/test_dominion.shp")
+    l_args = l_parser.parse_args()
+    return l_args
+
+
+def close_dominion(p_input_shapefile, p_bbox, p_threshold, p_sea_points):
+    file = p_input_shapefile
+
+    if p_bbox:
+        data_frame = geopandas.read_file(file, bbox=[float(coord) for coord in p_bbox])
+    else:
+        data_frame = geopandas.read_file(file)
+
+    # Merging parts of shapefile
+    matrix = [list(part.coords) for part in data_frame.geometry]
+    matrix = merge_parts(matrix, float(p_threshold))
+
+    # Creating dataframe
+    linestrings = [LineString(line) for line in matrix]
+    new_data_frame = geopandas.GeoDataFrame({"geometry": linestrings})
+
+    # Creation of dominion
+    sea_points = []
+
+    for string in p_sea_points:
+        a, b = string.split(",")
+        coords = (float(a), float(b))
+        sea_points.append(coords)
+
+    # ok for 1,2, and 3 point of dominion bbox (no model over islands, we have a coastline)
+    index_of_coastline = [idx for idx, part in enumerate(new_data_frame.geometry) if not part.is_ring][0]
+    matrix = [list(part.coords) for part in new_data_frame.geometry]
+
+    coastline = matrix[index_of_coastline]
+    start_coast = coastline[0]
+    end_coast = coastline[-1]
+
+    distances_from_start = [dist(start_coast, sea_point).km for sea_point in sea_points]
+    coastline.insert(0, sea_points[distances_from_start.index(min(distances_from_start))])
+
+    distances_from_end = [dist(end_coast, sea_point).km for sea_point in sea_points]
+    copy_of_distances = distances_from_end.copy()
+    while copy_of_distances:
+        coastline.append(sea_points[distances_from_end.index(min(copy_of_distances))])
+        copy_of_distances.remove(min(copy_of_distances))
+
+    matrix[index_of_coastline] = smooth_vertex(coastline, len(sea_points))
+
+    linestrings = [LineString(line) for line in matrix]
+    return geopandas.GeoDataFrame({"geometry": linestrings})
+
+
+def smooth_vertex(p_coastline, number_sea_points):
+    vicinity_factor = 25
+    n = len(p_coastline)
+    for i in range(1, number_sea_points+1):
+        value_vertex_1 = p_coastline[n-i]
+        value_vertex_2 = p_coastline[n-i-1]
+        value_vertex_3 = p_coastline[n-i-2]
+
+        value_vertex_1_2 = ((value_vertex_1[0]+value_vertex_2[0]*(vicinity_factor-1))/vicinity_factor,
+                            (value_vertex_1[1]+value_vertex_2[1]*(vicinity_factor-1))/vicinity_factor)
+        value_vertex_2_3 = ((value_vertex_3[0]+value_vertex_2[0]*(vicinity_factor-1))/vicinity_factor,
+                            (value_vertex_3[1]+value_vertex_2[1]*(vicinity_factor-1))/vicinity_factor)
+
+    return p_coastline
+
+
+def prompt_for_data(p_args):
+    change_bbox = input("\nWould you like to change bounding box? Y or N ")
+    change_sea_points = input("\nWould you like to change sea points? Y or N ")
+    sea_points = []
+    if change_bbox.lower() == "y":
+        bbox = [
+            input("\nInsert longitude of NE point "),
+            input("\nInsert latitude of NE point "),
+            input("\nInsert longitude of SW point "),
+            input("\nInsert latitude of SW point ")
+        ]
+        setattr(args, "bbox", bbox)
+    if change_sea_points.lower() == "y":
+        another_point = "Y"
+        while another_point.lower() == "y":
+            point = "{},{}".format(
+                input("\nInsert longitude of sea point "),
+                input("\nInsert latitude of sea point ")
+            )
+            sea_points.append(point)
+            another_point = input("\nDo you need another sea point? Y or N ")
+        setattr(args, "sea_points", sea_points)
+    return p_args
 
 
 def arrange_parts(i_part_one, i_part_two, situation):
@@ -20,7 +132,6 @@ def arrange_parts(i_part_one, i_part_two, situation):
 
 # FUNCTION TO MERGE PARTS OF SHAPEFILE
 def merge_parts(io_matrix, i_threshold=0.001):
-    print("iteration")
     len_matrix = len(io_matrix)
     index = 0
     while index < len(io_matrix):
@@ -52,79 +163,33 @@ def merge_parts(io_matrix, i_threshold=0.001):
     return merge_parts(io_matrix, i_threshold)
 
 
+def plot_shapefile(p_dataframe):
+    plt.figure()
+    for shape in [list(part.coords) for part in p_dataframe.geometry]:
+        x = [i[0] for i in shape]
+        y = [i[1] for i in shape]
+        plt.plot(x, y)
+    plt.show()
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    # defaults are for Fiumicino for testing reasons. For Qatar:
-    # python bpy_test.py -i "./shp/qatar-line.shp" -s 56.441,25.533 56.525,24.8
-    # or for test with bounding box:
-    # python bpy_test.py -i "./shp/qatar-line.shp" -s 56.441,25.4 56.525,25.000 -b 56.332 25.4 56.525 25.000
+    args = create_parser()
+    input_shapefile = args.input_shapefile
+    dominion_file = args.output_shapefile
 
-    parser.add_argument("-i", "--input_shapefile", help="Shapefile to use as input", default="./shp/fiumicino-line.shp")
-    parser.add_argument("-b", "--bbox", nargs="+",
-                        help="Bounding box for the shapefile. e.g. lon_up_sx lat_up_sx lon_down_dx lat_down_dx")
-    parser.add_argument("-s", "--sea_points", nargs="+",
-                        help="List of sea points where to close the dominion. e.g. lon1,lat1 lon2,lat2 ...",
-                        default=["11.992,41.872", "12.080,41.680"])
-    parser.add_argument("-t", "--threshold",
-                        help="Threshold for the minimum distance to merge shapefile parts and remove double points",
-                        default="0.001")
-    parser.add_argument("-o", "--output_shapefile", help="Shapefile to use as output",
-                        default="./shp/test_dominion.shp")
-    args = parser.parse_args()
-    file = args.input_shapefile
-    print(args.bbox)
-    if args.bbox:
-        data_frame = geopandas.read_file(file, bbox=[float(coord) for coord in args.bbox])
-    else:
-        data_frame = geopandas.read_file(file)
-
-    # Merging parts of shapefile
-    matrix = [list(part.coords) for part in data_frame.geometry]
-    matrix = merge_parts(matrix, float(args.threshold))
-
-    # Creating dataframe
-    linestrings = [LineString(line) for line in matrix]
-    new_data_frame = geopandas.GeoDataFrame({"geometry": linestrings})
-
-    # Creation of dominion
-    sea_points = []
-
-    for string in args.sea_points:
-        a, b = string.split(",")
-        coords = (float(a), float(b))
-        sea_points.append(coords)
-
-    print(sea_points)
-
-    # ok for 1,2, and 3 point of dominion bbox (no model over islands, we have a coastline)
-    index_of_coastline = [idx for idx, part in enumerate(new_data_frame.geometry) if not part.is_ring][0]
-    matrix = [list(part.coords) for part in new_data_frame.geometry]
-
-    coastline = matrix[index_of_coastline]
-    start_coast = coastline[0]
-    end_coast = coastline[-1]
-
-    distances_from_start = [dist(start_coast, sea_point).km for sea_point in sea_points]
-    print(distances_from_start)
-    coastline.insert(0, sea_points[distances_from_start.index(min(distances_from_start))])
-
-    distances_from_end = [dist(end_coast, sea_point).km for sea_point in sea_points]
-    print(distances_from_end)
-    copy_of_distances = distances_from_end.copy()
-    while copy_of_distances:
-        coastline.append(sea_points[distances_from_end.index(min(copy_of_distances))])
-        copy_of_distances.remove(min(copy_of_distances))
-
-    matrix[index_of_coastline] = coastline
-
-    linestrings = [LineString(line) for line in matrix]
-    dominion_data_frame = geopandas.GeoDataFrame({"geometry": linestrings})
+    is_fine = "N"
+    while is_fine.lower() != "y":
+        dominion_data_frame = close_dominion(input_shapefile, args.bbox, args.threshold, args.sea_points)
+        plot_shapefile(dominion_data_frame)
+        is_fine = input("\nIs this fine? Y or N ")
+        if is_fine.lower() != "y":
+            args = prompt_for_data(args)
 
     try:
-        dominion_file = "./shp/test_dominion.shp"
         dominion_data_frame.to_file(dominion_file)
     except ValueError:
         print("Check your bbox, it seems the coastline is not there.")
+
 
 # file = "./shp/fiumicino-line.shp"
 
